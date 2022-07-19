11685eaad7d301d671166678440a484fff59cbe1
f-i-t-s-u-m <fibabirr@gmail.com>
2022-07-19 12:41:02 +0300

    add task 1

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" flask app file """
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """ app config class """
+    LANGUAGES = ['en', 'fr']
+    
+
+@app.route('/')
+def index():
+    """ index route """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
