2217a8ab77369277cac11c383f25118c1af4729c
f-i-t-s-u-m <fibabirr@gmail.com>
2022-07-20 01:25:13 +0300

    fix output

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -49,12 +49,15 @@ def before_request():
 def get_locale():
     """ return best match language """
     loc = request.args.get('locale')
-    if loc and loc in app.config['LANGUAGES']:
-        return str(loc)
-    elif g.user['locale'] in app.config['LANGUAGES']:
-        return g.user['locale']
-    elif request.headers.get('locale') in app.config['LANGUAGES']:
-        return request.headers.get('locale')
+    if loc in app.config['LANGUAGES']:
+        return loc
+    if g.user:
+        loc = g.user.get('locale')
+        if loc and loc in app.config['LANGUAGES']:
+            return loc
+    loc = request.headers.get('locale', None)
+    if loc in app.config['LANGUAGES']:
+        return loc
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
