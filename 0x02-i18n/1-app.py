f4b6903e38847b8166ea099eec3f5aa099faf961
f-i-t-s-u-m <fibabirr@gmail.com>
2022-07-19 13:06:02 +0300

    link config object to fask app

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -4,16 +4,17 @@
 from flask import Flask, render_template
 from flask_babel import Babel
 
-app = Flask(__name__)
-babel = Babel(app)
-
 
 class Config:
     """ app config class """
     LANGUAGES = ['en', 'fr']
-    local = 'en'
-    timezone = 'UTC'
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
 
 @app.route('/')
 def index():
