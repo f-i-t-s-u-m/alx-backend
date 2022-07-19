91ebad82a62e8fb8fde82ccd8e8d10d1bb629ec4
f-i-t-s-u-m <fibabirr@gmail.com>
2022-07-19 21:56:46 +0300

    fix requirment

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -23,7 +23,7 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def index():
     """ index route """
     return render_template('3-index.html')
