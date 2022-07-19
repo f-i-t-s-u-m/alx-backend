3121791e967ea7a7fbae236567c7ddb9d2a6f0b6
f-i-t-s-u-m <fibabirr@gmail.com>
2022-07-19 14:00:51 +0300

    fix jinja gettext

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -8,7 +8,7 @@ from flask_babel import Babel, gettext
 class Config:
     """ app config class """
     LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_LOCALE = 'fr'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
