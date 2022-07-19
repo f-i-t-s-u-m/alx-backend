b16586878fd86a42689a5f07bb111bc9cb473ae2
f-i-t-s-u-m <fibabirr@gmail.com>
2022-07-19 21:57:46 +0300

    fix requirment

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 """ flask app file """
 
-from flask import Flask, render_template, request
+from flask import (
+        Flask,
+        render_template,
+        request
+)
 from flask_babel import Babel, gettext
 
 
